89cb4b1c2dec7cdb61d3629a0c0b590d01457de7
Jireh Flores <Jirekcz03ft@gmail.com>
2024-09-24 13:23:23 -0500

    Carga inicial backend

---
+++ src/main/java/com/example/demo/T2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2BackendApplication.class, args);
+	}
+
+}
